bae6b80d2f893cd42eb4db7437bf510b16840efe
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-03-23 10:09:58 +0600

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,42 @@
+const productEl = document.getElementById("product");
+const quantityEl = document.getElementById("quantity");
+const cartContainer = document.getElementById("cart-container");
+
+document.getElementById("btn-add").addEventListener("click", function () {
+
+  // get value
+ const productName = productEl.value;
+const quantity = parseInt(quantityEl.value);
+
+  // create div for adding content
+  const cartContent = document.createElement("div");
+  cartContent.classList.add("bg-base-200", "mt-5", "shadow" ,"rounded-md", "p-4");
+  cartContent.innerText = `
+  product name: ${productName} and quantity: ${quantity}
+  `
+  // cart add
+  cartContainer.appendChild(cartContent);
+  setToStorage(productName, quantity);
+  
+  productEl.value = "";
+  quantityEl.value = "";
+});
+
+function setToStorage(product, quantity) {
+// get old cart
+ let cart = localStorage.getItem("cart");
+ 
+  if(cart){
+    let cartObj =  JSON.parse(cart);
+    cartObj[product] += quantity;
+ 	let cart = JSON.stringify(cartObj);
+ 	localStorage.setItem("cart", cart);
+  }else{
+    // set new item
+  let cartObj = {};
+  cartObj[product] = quantity;
+  let cart = JSON.stringify(cartObj);
+  localStorage.setItem(" cart", cart);
+  }
+  
+}
